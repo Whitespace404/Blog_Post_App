9ee4ac48a838b15b21387fa8ae5e329fd1164cb1
Whitespace404 <Whitespace404>
2020-08-15 12:59:34 +0530

    Styled pagination buttons

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,11 +8,11 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'opr4o0r23jk3423f04h02in2q99lzqf1gp0-0ahld23s05bdqm0oir'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config["MAIL_SERVER"] = 'smtp.googlemail.com'
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "your email id"
-app.config["MAIL_PASSWORD"] = 'your email password'
+app.config["MAIL_USERNAME"] = "enter your email id here"
+app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PWD')
 
 
 db = SQLAlchemy(app)
