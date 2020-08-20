cad7e79be1cb629f8e43909afccfd0cab6862555
Whitespace404 <Whitespace404>
2020-08-20 20:20:55 +0530

    Minor changes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "rush2asharahul@gmail.com"
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_ID')
 app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PWD')
 
 # Instances
